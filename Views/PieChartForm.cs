3f8d57ec638dfc07179efc280193ee4da9d1d72f
Luis Otero <alonsooteroseminario@gmail.com>
2021-04-22 19:10:23 -0400

    Ordenar info a mostrar: menos decimales y borrar porcentajes. Etiquetas de colores, moverlas para afuera del grafico Piechart

--- Views/PieChartForm.cs
+++ Views/PieChartForm.cs
@@ -2,13 +2,8 @@
 using LiveCharts.Wpf;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Media;
 
 namespace AddinExportCDW.Views
 {
@@ -18,6 +13,7 @@ namespace AddinExportCDW.Views
         private List<Dictionary<string, string>> lista_Dictionarios_NEW { get; set; }
         private List<double> lista_desperdicios_NEW { get; set; }
         private double desperdicioTotal_NEW { get; set; }
+
         public PieChartForm(List<double> listaN_valor,
                              List<Dictionary<string, string>> lista_Dictionarios,
                              List<double> lista_desperdicios,
@@ -32,88 +28,40 @@ namespace AddinExportCDW.Views
 
         private void PieChartForm_Load(object sender, EventArgs e)
         {
-            Func<ChartPoint, string> labelPoint = chartPoint => string.Format("{0} ({1:P})", chartPoint.Y, chartPoint.Participation);
-            pieChart1.InnerRadius = 100;
-            // Define the collection of Values to display in the Pie Chart
-            pieChart1.Series = new SeriesCollection
+            Func<ChartPoint, string> labelPoint = chartPoint => string.Format("{0} m3   ({1:P})", chartPoint.Y, chartPoint.Participation);
+
+            pieChart1.InnerRadius = 75;
+
+            List<string> keys = Dictionary.DictionaryListKeys(Dictionary.Get("data_forjado"));
+            int i = 0;
+            foreach (var item in listaN_valor_NEW)
             {
-
-                new PieSeries
+                if (item < 1)
                 {
-                    Title = "07 07 01 - aqueous washing liquids",
-                    Values = new ChartValues<double> { listaN_valor_NEW[0] },
-                    DataLabels = true,
-                    LabelPoint = labelPoint,
-                    Fill = System.Windows.Media.Brushes.Green
-                },
-                new PieSeries
-                {
-                    Title = "15 01 02 - plastic packaging",
-                    Values = new ChartValues<double> { listaN_valor_NEW[1] },
-                    DataLabels = true,
-                    LabelPoint = labelPoint
-                },
-                new PieSeries
-                {
-                    Title = "15 01 03 - wooden packaging",
-                    Values = new ChartValues<double> { listaN_valor_NEW[2] },
-                    DataLabels = true,
-                    LabelPoint = labelPoint,
-                    //PushOut = 15,
-                },
-                new PieSeries
-                {
-                    Title = "15 01 04 - metallic packaging",
-                    Values = new ChartValues<double> { listaN_valor_NEW[3] },
-                    DataLabels = true,
-                    LabelPoint = labelPoint,
-                    Fill = System.Windows.Media.Brushes.Gray
-                },
-                new PieSeries
-                {
-                    Title = "15 01 06 - mixed packaging",
-                    Values = new ChartValues<double> { listaN_valor_NEW[4] },
-                    DataLabels = true,
-                    LabelPoint = labelPoint
-                },
-                new PieSeries
-                {
-                    Title = "17 01 01 - concrete",
-                    Values = new ChartValues<double> { listaN_valor_NEW[5] },
-                    DataLabels = true,
-                    LabelPoint = labelPoint
-                },
-                new PieSeries
-                {
-                    Title = "17 02 01 - wood",
-                    Values = new ChartValues<double> { listaN_valor_NEW[6] },
-                    DataLabels = true,
-                    LabelPoint = labelPoint
-                },                
-                new PieSeries
-                {
-                    Title = "17 02 03 - plastic",
-                    Values = new ChartValues<double> { listaN_valor_NEW[7] },
-                    DataLabels = true,
-                    LabelPoint = labelPoint
-                },                
-                new PieSeries
-                {
-                    Title = "17 04 05 - iron and steel",
-                    Values = new ChartValues<double> { listaN_valor_NEW[8] },
-                    DataLabels = true,
-                    LabelPoint = labelPoint
-                },                
-                new PieSeries
-                {
-                    Title = "17 09 04 - mixed",
-                    Values = new ChartValues<double> { listaN_valor_NEW[9] },
-                    DataLabels = true,
-                    LabelPoint = labelPoint
+                    pieChart1.Series.Add(new PieSeries
+                    {
+                        Title = keys[i],
+                        Values = new ChartValues<double> { Math.Round(item, 2) },
+                        LabelPoint = labelPoint,
+                        LabelPosition = PieLabelPosition.OutsideSlice,
+                        Foreground = Brushes.Black,
+                        FontSize = 12
+                    });
                 }
-            };
-
-            // Set the legend location to appear in the bottom of the chart
+                else
+                {
+                    pieChart1.Series.Add(new PieSeries
+                    {
+                        Title = keys[i],
+                        Values = new ChartValues<double> { Math.Round(item, 2) },
+                        DataLabels = true,
+                        LabelPoint = labelPoint,
+                        LabelPosition = PieLabelPosition.InsideSlice,
+                        FontSize = 12
+                    });
+                }
+                i++;
+            }
             pieChart1.LegendLocation = LegendLocation.Right;
         }
     }
